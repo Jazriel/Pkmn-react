709ad5659825c1134741d770eb9358c9c1e5667c
Jazriel <javyermartinez@gmail.com>
2020-03-11 16:49:33 +0100

    Fixed failing dependency

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,25 +1,12 @@
 import React, { useState } from 'react';
-import {createUseStyles} from 'react-jss';
 
 import TypeList from './TypeList';
 
-
-const useStyles = createUseStyles({
-  PokemonCard: {
-    'box-sizing': 'border-box',
-    padding: '1em',
-    margin: '0.2em',
-    border: '1px #ccc solid',
-    'border-radius': '5px',
-  }
-});
 
 export default ({pokemon}) => {
   const [shiny, setShiny] = useState(false);
   const [front, setFront] = useState(true);
   const sprite = (front ? 'front': 'back') + '_' + (shiny ? 'shiny': 'default');
-
-  const styles = useStyles();
 
   return (
     <div className='PokemonCard center'>
